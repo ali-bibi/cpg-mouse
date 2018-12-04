5b06327174445f74fae251c85b30a431af1d217c
Ali Bibi <ali-bibi@users.noreply.github.com>
2018-12-04 15:55:19 +0100

    Deleted irrelevant folder

--- cpg_mouse_0/all_neurons_spike_monitor.py
+++
@@ -1,4 +0,0 @@
-# Imported Python Transfer Function
-@nrp.NeuronMonitor(nrp.brain.record[slice(0, 90, 1)], nrp.spike_recorder)
-def all_neurons_spike_monitor(t):
-    return True
--- cpg_mouse_0/monitor_actors.py
+++
@@ -1,8 +0,0 @@
-@nrp.MapSpikeSink("right_actor", nrp.brain.actors[0], nrp.leaky_integrator_alpha)
-@nrp.MapSpikeSink("left_actor", nrp.brain.actors[1], nrp.leaky_integrator_alpha)
-@nrp.MapRobotPublisher("rm_topic", Topic("/robot/forearm_R_joint/cmd_pos", std_msgs.msg.Float64))
-@nrp.MapRobotPublisher("lm_topic", Topic("/robot/forearm_L_joint/cmd_pos", std_msgs.msg.Float64))
-@nrp.Neuron2Robot()
-def monitor_actors (t, right_actor, left_actor, rm_topic, lm_topic):
-    rm_topic.send_message(int(right_actor.voltage * 1e3))
-    lm_topic.send_message(int(left_actor.voltage * 1e3))
